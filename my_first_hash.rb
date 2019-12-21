509ebf1eca2d038e1bacb04b462fa61715efe56d
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-21 05:51:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,32 +1,32 @@
 def my_hash
-  your_hash = {"name" => "Asha"}
+  your_hash = ("name" => "Asha")
 end
 
 def shipping_manifest
 
-  the_manifest = {
+  the_manifest = (
       "whale bone corsets" =>5, 
       "porcelain vases" =>2, 
       "oil paintings" =>3
-    }
+  )
 end
 
 def retrieval
-  shipping_manifest = {
+  shipping_manifest = (
                   "whale bone corsets" =>5, 
                   "porcelain vases" =>2, 
-                  "oil paintings" =>3
-                 }
+                  "oil paintings" =>3 )
+                 
 
   shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
+  shipping_manifest = (
                   "whale bone corsets" =>5, 
                   "porcelain vases" =>2, 
                   "oil paintings" =>3
-                 }
+                 )
 
   shipping_manifest["muskets"] = 2
 
