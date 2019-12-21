08b38144508c3bb744cd73b98038c306e068969a
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-21 05:50:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,17 +5,17 @@ end
 def shipping_manifest
 
   the_manifest = {
-      "whale bone corsets" => 5, 
-      "porcelain vases" => 2, 
-      "oil paintings" => 3
+      "whale bone corsets" =>5, 
+      "porcelain vases" =>2, 
+      "oil paintings" =>3
     }
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale bone corsets" =>5, 
+                  "porcelain vases" =>2, 
+                  "oil paintings" =>3
                  }
 
   shipping_manifest["oil paintings"]
@@ -23,9 +23,9 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale bone corsets" =>5, 
+                  "porcelain vases" =>2, 
+                  "oil paintings" =>3
                  }
 
   shipping_manifest["muskets"] = 2
